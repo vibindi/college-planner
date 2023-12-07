bc7c0b495230d7d4adbd883bfd0a9fd1f5a0f2e1
vibindi <v.bindiganavile@gmail.com>
2023-12-07 02:38:35 -0500

    final code submission for stage 2

--- app.py
+++ app.py
@@ -15,25 +15,25 @@ db = SQLAlchemy(app)
 class Users(db.Model):
   user_id = db.Column(db.Integer, primary_key=True)
   name = db.Column(db.String(200), nullable=False)
-  email = db.Column(db.String(200), nullable=False)
-  curr_semester_id = db.Column(db.Integer, nullable=False)
+  email = db.Column(db.String(200), nullable=False, index=True)
+  curr_semester_id = db.Column(db.Integer, nullable=False, index=True)
 
 class Semesters(db.Model):
   semester_id = db.Column(db.Integer, primary_key=True)
-  user_id = db.Column(db.Integer, nullable=False)
+  user_id = db.Column(db.Integer, nullable=False, index=True)
   season = db.Column(db.String(200), nullable=False)
   year = db.Column(db.Integer, nullable=False)
   
 class Courses(db.Model):
   course_id = db.Column(db.Integer, primary_key=True)
-  semester_id = db.Column(db.Integer, nullable=False)
+  semester_id = db.Column(db.Integer, nullable=False, index=True)
   course_prefix = db.Column(db.String(200), nullable=False)
   course_number = db.Column(db.String(200), nullable=False)
   course_name = db.Column(db.String(200), nullable=False)
 
 class Assignments(db.Model):
   assignment_id = db.Column(db.Integer, primary_key=True)
-  course_id = db.Column(db.Integer, nullable=False)
+  course_id = db.Column(db.Integer, nullable=False, index=True)
   name = db.Column(db.String(200), nullable=False)
   description = db.Column(db.String(200), nullable=False)
   due_date = db.Column(db.DateTime, nullable=False)
@@ -42,7 +42,7 @@ class Assignments(db.Model):
 
 class Exams(db.Model):
   exam_id = db.Column(db.Integer, primary_key=True)
-  course_id = db.Column(db.Integer, nullable=False)
+  course_id = db.Column(db.Integer, nullable=False, index=True)
   name = db.Column(db.String(200), nullable=False)
   exam_date = db.Column(db.DateTime, nullable=False)
   exam_location = db.Column(db.String(200), nullable=False)
